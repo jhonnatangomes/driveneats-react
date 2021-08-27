df54d8e3a8ff163fadad75b4fcae27ff865a517b
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2021-08-26 22:44:21 -0300

    Item selection implemented

--- src/components/categorias.js
+++ src/components/categorias.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function Categorias(props){
     return (
         <div className={props.pratos.categoria}>
@@ -16,13 +18,41 @@ export default function Categorias(props){
 }
 
 function Item(props) {
+
+    const [classeDiv, setClasseDiv] = useState("item roboto");
+    const [classeContador, setClasseContador] = useState("contador escondido");
+    const [contador, setContador] = useState(1);
+
+    function selecionar() {
+        setClasseDiv("item roboto selecionado");
+        setClasseContador("contador");
+    }
+
+    function alterarContador(e, button) {
+        if(button === "+") setContador(contador + 1);
+        else setContador(contador - 1);
+        
+        if(contador === 1 && button === "-") {
+            setClasseDiv("item roboto");
+            setClasseContador("contador escondido");
+        }
+
+        e.stopPropagation();
+        
+    }
+
     return (
-        <div className="item roboto prato" onclick="selecionar_prato(this);">
+        <div className={classeDiv} onClick={selecionar}>
                     <img src={props.item.image} alt={props.item.descricao} />
                     <p className="nome-item">{props.item.nome}</p>
                     <p className="descricao-item">{props.item.descricao}</p>
                     <p className="preco">{props.item.preco}</p>
-                    <ion-icon name="checkmark-circle"></ion-icon>
+                    <div className={classeContador}>
+                        <button onClick={(e) => {alterarContador(e, "-")}}>-</button>
+                        <div>{contador}</div>
+                        <button onClick={(e) => {alterarContador(e, "+")}}>+</button>
+                    </div>
                 </div>
     );
 }
+
